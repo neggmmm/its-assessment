ab8162cb5b0470a542139b392601c6d711a18a57
neggmmm <kekkdead@gmail.com>
2026-04-30 23:15:52 +0300

    CORS Origin production

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ import submissionRouter from './modules/submissions/submission.route.ts';
 const app = express();
 
 app.use(cors({
-  origin: config.frontendUrl,
+  origin: ['http://localhost:5173','https://exam-its.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
