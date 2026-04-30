b170eb8072a51baaec257ff77f889217efe9a565
neggmmm <kekkdead@gmail.com>
2026-04-30 23:07:49 +0300

    db config to production

--- server/src/configuration/db.config.ts
+++ server/src/configuration/db.config.ts
@@ -1,13 +1,12 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: 'postgres',
-    logging: false,
-  }
-);
+export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
